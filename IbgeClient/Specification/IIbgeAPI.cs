aca0349ef2cfc4cc103cb4df86c9e5fe1b7e9c61
PauloZier <paulozier42@gmail.com>
2021-01-31 20:55:44 -0300

    Gerando pacotes NuGET

---
+++ IbgeClient/Specification/IIbgeAPI.cs
@@ -0,0 +1,28 @@
+using Models;
+using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IbgeClient.Specification
+{
+    public interface IIbgeAPI
+    {
+        [Get("/distritos")]
+        Task<IList<Distrito>> GetDistritos();
+
+        [Get("/municipios")]
+        Task<IList<Municipio>> GetMunicipios();
+
+        [Get("/microrregioes")]
+        Task<IList<Microrregiao>> GetMicrorregioes();
+
+        [Get("/mesorregioes")]
+        Task<IList<Mesorregiao>> GetMesorregioes();
+
+        [Get("/estados")]
+        Task<IList<Estado>> GetEstados();
+
+        [Get("/regioes")]
+        Task<IList<Regiao>> GetRegioes();
+    }
+}
--- Tests/IbgeAPITests.cs
+++ Tests/IbgeAPITests.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using Services;
+using IbgeClient;
 using Models;
 using System.Collections.Generic;
 
